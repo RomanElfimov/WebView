71708ed19289541bca23fa37a4db3a521ebb8b1e
Роман <elfiroma@gmail.com>
2020-05-22 16:13:13 +0300

    Initial Commit

---
+++ WebViewWithLaunchScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebViewWithLaunchScreen
+//
+//  Created by Роман Елфимов on 22.05.2020.
+//  Copyright © 2020 Роман Елфимов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
